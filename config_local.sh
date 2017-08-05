be7cb6870db73a30595969c1d2396e88fd14e156
David Hodo <david.hodo@is4s.com>
2017-08-05 12:00:37 -0500

    add local config file

---
+++ config_local.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+export ANDROID_HOME=/home/davidhodo/android
+export ANDROID_NDK=$ANDROID_HOME/ndk-bundle
+export PATH=$PATH:$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$ANDROID_HOME/ndk-bundle/build/tools
+
